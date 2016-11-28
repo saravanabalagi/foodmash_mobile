bc3a1e13c066a4aa74cae2e0738330bf9e4c1e50
Saravanabalagi R <saravanabalagi@hotmail.com>
2016-11-28 20:32:10 +0530

    Avoiding mutation of states

--- src/reducers/user/userReducer.js
+++ src/reducers/user/userReducer.js
@@ -1,5 +1,4 @@
 export default (state = {
-    jwt: null,
     inProgress: false,
     name: null,
     mobile: null,
@@ -25,17 +24,38 @@ export default (state = {
 },action) => {
     const newState = {...state};
     switch(action.type) {
-        case "FETCH_JWT_IN_PROGRESS": newState.session.inProgress = true; break;
-        case "FETCH_JWT_FULFILLED": newState.session.jwt = action.payload; newState.session.error = null; newState.session.inProgress = false; break;
-        case "FETCH_JWT_FAILED": newState.session.jwt = null; newState.session.error = action.payload; newState.session.inProgress = false; break;
-
         case "FETCH_USER_IN_PROGRESS": newState.inProgress = true; break;
         case "FETCH_USER_FULFILLED": newState.user = action.payload; newState.error = null; newState.inProgress = false; break;
         case "FETCH_USER_FAILED": newState.error = action.payload; newState.inProgress = false; break;
+    }
+    if(["FETCH_JWT_IN_PROGRESS",
+            "FETCH_JWT_FULFILLED",
+            "FETCH_JWT_FAILED"].includes(action.type))
+        newState.session = manageJwt(newState.session,action);
 
-        case "SIGNUP_USER_IN_PROGRESS": newState.session.signup.inProgress = true; break;
-        case "SIGNUP_USER_FULFILLED": newState.session.signup.user = action.payload; newState.session.signup.error = null; newState.session.signup.inProgress = false; break;
-        case "SIGNUP_USER_FAILED": newState.session.signup.error = action.payload; newState.session.signup.inProgress = false; break;
+    else if(["SIGNUP_USER_IN_PROGRESS",
+            "SIGNUP_USER_FULFILLED",
+            "SIGNUP_USER_FAILED"].includes(action.type))
+        newState.session = {...newState.session, signup: manageSignup(newState.session.signup,action)};
+    return newState;
+};
+
+let manageJwt = (state={}, action) => {
+    const newState = {...state};
+    switch(action.type) {
+        case "FETCH_JWT_IN_PROGRESS": newState.inProgress = true; break;
+        case "FETCH_JWT_FULFILLED": newState.jwt = action.payload; newState.error = null; newState.inProgress = false; break;
+        case "FETCH_JWT_FAILED": newState.jwt = null; newState.error = action.payload; newState.inProgress = false; break;
+    }
+    return newState;
+};
+
+let manageSignup = (state={}, action) => {
+    const newState = {...state};
+    switch(action.type) {
+        case "SIGNUP_USER_IN_PROGRESS": newState.inProgress = true; break;
+        case "SIGNUP_USER_FULFILLED": newState.user = action.payload; newState.error = null; newState.inProgress = false; break;
+        case "SIGNUP_USER_FAILED": newState.error = action.payload; newState.inProgress = false; break;
     }
     return newState;
 };
