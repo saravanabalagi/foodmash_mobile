fa091658fcdc7967052f913576627e0c4a2266e0
Saravanabalagi R <saravanabalagi@hotmail.com>
2016-11-28 22:29:10 +0530

    Passing id to reducers

--- src/reducers/dish/dishActions.js
+++ src/reducers/dish/dishActions.js
@@ -3,9 +3,9 @@ import axios from 'axios';
 export function fetchDish(id) {
     const url = '/dishes/'+id.toString();
     return (dispatch) => {
-        dispatch({type: "FETCH_DISH_IN_PROGRESS"});
+        dispatch({type: "FETCH_DISH_IN_PROGRESS", id: id});
         axios.get(url)
-            .then((response) => { dispatch({ type: "FETCH_DISH_FULFILLED", payload: response.data}); })
-            .catch((error) => { dispatch({ type: "FETCH_DISH_FAILED", payload: error }); });
+            .then((response) => { dispatch({ type: "FETCH_DISH_FULFILLED", payload: response.data, id: id }); })
+            .catch((error) => { dispatch({ type: "FETCH_DISH_FAILED", payload: error, id: id }); });
     };
 }
