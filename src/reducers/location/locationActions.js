4ba2966813daa06417d0df3645341452f18ee646
Zeke <saravanabalagi@hotmail.com>
2017-01-20 13:15:32 +0530

    Removing siblings

--- src/reducers/location/locationActions.js
+++ src/reducers/location/locationActions.js
@@ -11,21 +11,10 @@ export function fetchLocations(city_id) {
     };
 }
 
-export function fetchSiblings(location_id) {
-    const url = '/locations/' + location_id.toString() + '/siblings';
-    return (dispatch) => {
-        dispatch({type: "FETCH_SIBLINGS_IN_PROGRESS"});
-        axios.get(url)
-            .then((response) => { dispatch({ type: "FETCH_SIBLINGS_FULFILLED", payload: response.data}); })
-            .catch((error) => { dispatch({ type: "FETCH_SIBLINGS_FAILED", payload: error }); });
-    };
-}
-
 export function selectLocation(location_id, city_id) {
     return (dispatch) => {
         dispatch({type: "SELECT_LOCATION", payload: location_id}) ;
         dispatch({type: "SELECT_CITY", payload: city_id});
-        dispatch(fetchSiblings(location_id));
         Actions.app();
     }
 }
--- src/reducers/location/locationReducer.js
+++ src/reducers/location/locationReducer.js
@@ -1,7 +1,6 @@
 export default (state = {
     locations: [],
     selected: null,
-    siblings: [],
     inProgress: false,
     error: null
 }, action) => {
@@ -10,10 +9,6 @@ export default (state = {
         case "FETCH_LOCATIONS_IN_PROGRESS": newState.inProgress = true; break;
         case "FETCH_LOCATIONS_FULFILLED": newState.locations = action.payload; newState.error = null; newState.inProgress = false; break;
         case "FETCH_LOCATIONS_FAILED": newState.error = action.payload; newState.inProgress = false; break;
-
-        case "FETCH_SIBLINGS_IN_PROGRESS": newState.inProgress = true; break;
-        case "FETCH_SIBLINGS_FULFILLED": newState.siblings = action.payload; newState.error = null; newState.inProgress = false; break;
-        case "FETCH_SIBLINGS_FAILED": newState.error = action.payload; newState.inProgress = false; break;
 
         case "SELECT_LOCATION": newState.selected = action.payload;
     }
