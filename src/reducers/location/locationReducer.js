433924f55dfe0caf73537015988d4d517ffe8344
Zeke <saravanabalagi@hotmail.com>
2017-02-01 16:26:54 +0530

    Changing to ids as keys instead of using arrays

--- src/reducers/location/locationReducer.js
+++ src/reducers/location/locationReducer.js
@@ -1,5 +1,5 @@
 export default (state = {
-    locations: [],
+    locations: {},
     selected: null,
     inProgress: false,
     error: null
@@ -7,10 +7,21 @@ export default (state = {
     const newState = {...state};
     switch(action.type) {
         case "FETCH_LOCATIONS_IN_PROGRESS": newState.inProgress = true; break;
-        case "FETCH_LOCATIONS_FULFILLED": newState.locations = action.payload; newState.error = null; newState.inProgress = false; break;
+        case "FETCH_LOCATIONS_FULFILLED": newState.locations = updateLocations(newState.locations, action); newState.error = null; newState.inProgress = false; break;
         case "FETCH_LOCATIONS_FAILED": newState.error = action.payload; newState.inProgress = false; break;
+
+        case "FETCH_LOCATION_IN_PROGRESS": newState.inProgress = true; break;
+        case "FETCH_LOCATION_FULFILLED": newState.locations = updateLocations(newState.locations, action); newState.error = null; newState.inProgress = false; break;
+        case "FETCH_LOCATION_FAILED": newState.error = action.payload; newState.inProgress = false; break;
 
         case "SELECT_LOCATION": newState.selected = action.payload;
     }
+    return newState;
+}
+
+function updateLocations(state = {},action) {
+    const newState = {...state};
+    if(Array.isArray(action.payload)) action.payload.forEach(location=> newState[location.id] = location);
+    else newState[action.payload.id] = action.payload;
     return newState;
 }
