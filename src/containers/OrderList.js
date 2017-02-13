b68977833343345bdaf6ddbb5e97b5dc454e48a6
Zeke <saravanabalagi@hotmail.com>
2017-02-13 23:47:13 +0530

    Adding noOrdersLayout

--- src/containers/OrderList.js
+++ src/containers/OrderList.js
@@ -11,6 +11,7 @@ import {connect} from 'react-redux';
 import {fetchOrders} from '../reducers/order/orderActions';
 import Order from '../views/Order';
 import Loading from '../views/Loading';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 @connect((store) => {
     return {
@@ -34,21 +35,34 @@ class OrderList extends React.Component {
 
     render() {
         return (
-            <ScrollView>
-                <View style={s.parent}>
-                    { this.props.inProgress && <Loading/> }
-                    {
-                        this.props.orders.map((order) => {
-                            return <Order
-                                selected={this.state.selectedOrder && order.id===this.state.selectedOrder.id}
-                                select={()=>this.selectOrder(order)}
-                                key={order.id}
-                                order={order} />
-                        })
-                    }
-                    { this.props.error != null && !this.props.inProgress && <Text> {this.props.error.toString()} </Text> }
-                </View>
-            </ScrollView>
+            <View style={s.parent}>
+                { this.props.inProgress && <Loading/> }
+                {
+                    this.props.orders.length === 0 &&
+                    <View style={s.noOrdersLayout}>
+                        <Icon name={"exclamation-triangle"} size={100} color={"#e16800"}/>
+                        <Text style={s.ordersEmpty}>You haven't ordered yet</Text>
+                        <Text style={s.getReady}>Get ready to eat now</Text>
+                    </View>
+                }
+                {
+                    this.props.orders.length>0 &&
+                    <ScrollView style={s.scrollableArea}>
+                        <View>
+                            {
+                                    this.props.orders.map((order) => {
+                                    return <Order
+                                        selected={this.state.selectedOrder && order.id === this.state.selectedOrder.id}
+                                        select={() => this.selectOrder(order)}
+                                        key={order.id}
+                                        order={order}/>
+                                })
+                            }
+                        </View>
+                    </ScrollView>
+                }
+                { this.props.error != null && !this.props.inProgress && <Text> {this.props.error.toString()} </Text> }
+            </View>
         );
 
     }
@@ -57,6 +71,22 @@ class OrderList extends React.Component {
 
 const s = StyleSheet.create({
     parent: {
+        flex: 1
+    },
+    noOrdersLayout: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    ordersEmpty: {
+        fontSize: 15,
+        marginTop: 20
+    },
+    getReady: {
+        fontSize: 17,
+        padding: 5
+    },
+    scrollableArea: {
         marginTop: 10
     }
 });
