5661ea4039c69dee87e437d294aef2b56fc0e823
Zeke <saravanabalagi@hotmail.com>
2017-02-15 13:33:30 +0530

    Ellipsize longer names

--- src/containers/UserDetails.js
+++ src/containers/UserDetails.js
@@ -39,7 +39,7 @@ export default class UserDetails extends Component {
                 <View style={s.userDetailsWrapper}>
                     <Icon style={s.icon} name={"contacts"} size={75} color={"#F37521"}/>
                     <View style={s.rightPane}>
-                        <Text style={s.name}>{this.props.name?this.props.name:"Name"} </Text>
+                        <Text style={s.name} numberOfLines={1}>{this.props.name?this.props.name:"Name"} </Text>
                         <Text style={s.email}>{this.props.email?this.props.email:"me@email.com"} </Text>
                         <Text style={s.mobile}>+91 {this.props.mobile?(this.props.mobile.substring(0,5) + " " + this.props.mobile.substring(5)):"99999 99999"} </Text>
                     </View>
@@ -64,7 +64,7 @@ const s = StyleSheet.create({
         paddingTop: 5,
         paddingBottom: 5
     },
-    rightPane: { justifyContent: 'space-between' },
+    rightPane: { justifyContent: 'space-between', flex: 1 },
     name: { fontSize: 25 },
     email: { fontSize: 13 },
     mobile: { fontSize: 17 },
