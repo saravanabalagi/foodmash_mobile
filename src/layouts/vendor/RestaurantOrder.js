0e10f7a01b72010f220a91f69068b2163a33f847
Zeke <saravanabalagi@hotmail.com>
2017-02-24 22:39:29 +0530

    Fetch RestaurantOrder independently RestaurantOrderAction Buttons Design Enh

--- src/layouts/vendor/RestaurantOrder.js
+++ src/layouts/vendor/RestaurantOrder.js
@@ -13,17 +13,21 @@ import moment from 'moment';
 
 import Icon from 'react-native-vector-icons/FontAwesome';
 import RestaurantOrderItem from '../../containers/vendor/RestaurantOrderItem';
+import Loading from '../../views/Loading';
 
+import {makeRestaurantOrder} from '../../reducers/vendor/restaurantOrder/restaurantOrderActions';
 import {fetchPaymentMethod} from '../../reducers/paymentMethod/paymentMethodActions';
+import {fetchRestaurantOrder} from '../../reducers/vendor/restaurantOrder/restaurantOrderActions';
 import {fetchOrderStatus} from '../../reducers/orderStatus/orderStatusActions';
-import {fetchRestaurant} from '../../reducers/restaurant/restaurantActions';
 
 @connect((store,props) => {
     return {
+        restaurantOrder: store.vendor.restaurantOrder.restaurantOrders[props.restaurantOrderId],
+        inProgress: store.vendor.restaurantOrder.inProgress,
         orderStatuses: store.orderStatus.orderStatuses,
+        paymentMethods: store.paymentMethod.paymentMethods,
         dishVariants: store.dishVariant.dishVariants,
         dishes: store.dish.dishes,
-        restaurants: store.restaurant.restaurants,
     };
 })
 
@@ -36,27 +40,63 @@ class RestaurantOrder extends React.Component {
     }
 
     componentWillMount = () => {
-        this.props.dispatch(fetchOrderStatus(this.props.restaurantOrder.order_status_id));
-        this.props.dispatch(fetchPaymentMethod(this.props.restaurantOrder.payment_method_id));
-        this.props.dispatch(fetchRestaurant(this.props.restaurantOrder.restaurant_id));
+        if(this.props.restaurantOrder) {
+            this.props.dispatch(fetchOrderStatus(this.props.restaurantOrder.order_status_id));
+            this.props.dispatch(fetchPaymentMethod(this.props.restaurantOrder.payment_method_id));
+        } else this.props.dispatch(fetchRestaurantOrder(this.props.restaurantOrderId))
     };
 
     componentWillReceiveProps = (nextProps) => {
-        if(this.props.restaurantOrder!==nextProps.restaurantOrder) {
-            this.props.dispatch(fetchOrderStatus(nextProps.restaurantOrder.order_status_id));
-            this.props.dispatch(fetchPaymentMethod(nextProps.restaurantOrder.payment_method_id));
-            this.props.dispatch(fetchRestaurant(nextProps.restaurantOrder.restaurant_id));
+        let restaurantOrder = nextProps.restaurantOrder;
+        let orderStatus = restaurantOrder?nextProps.orderStatuses[restaurantOrder.order_status_id]:null;
+        let paymentMethod = restaurantOrder?nextProps.paymentMethods[restaurantOrder.paymentMethod_id]:null;
+
+        if(restaurantOrder && orderStatus==null) this.props.dispatch(fetchOrderStatus(nextProps.restaurantOrder.order_status_id));
+        if(restaurantOrder && paymentMethod==null) this.props.dispatch(fetchPaymentMethod(nextProps.restaurantOrder.payment_method_id));
+    };
+
+    getOrderStatus = () => { return this.props.restaurantOrder?this.props.orderStatuses[this.props.restaurantOrder.order_status_id]:null };
+    getPaymentMethod = () => { return this.props.restaurantOrder?this.props.paymentMethods[this.props.restaurantOrder.payment_method_id]:null };
+
+    getIconForOrderStatus = (status) => {
+        switch (status) {
+            case "Accepted": return "spinner";
+            case "Ready": return "bell";
+            case "Purchased": return "inr";
+            case "Completed": return "check";
+            case "Rejected": return "times";
         }
+        return "question-circle";
+    };
+
+    getIconForPaymentMethod = (method) => {
+        switch (method) {
+            case "COD": return "money";
+            case "Netbanking": return "globe";
+            case "Card": return "credit-card";
+        }
+        return "question-circle";
     };
 
     getStatusChangeButtonName = () => {
-        if(this.props.orderStatus)
-            switch (this.props.orderStatus.name) {
+        if(this.getOrderStatus())
+            switch (this.getOrderStatus().name) {
                 case "Purchased": return "APPROVE";
                 case "Accepted": return "READY";
                 case "Ready": return "COLLECTED";
             }
         return "Loading";
+    };
+
+    handleAction = (action) => {
+        if(action!="reject")
+            switch(this.getOrderStatus().name) {
+                case "Purchased": action="approve"; break;
+                case "Accepted": action="ready"; break;
+                case "Ready": action="collected"; break;
+                default: return
+            }
+        this.props.dispatch(makeRestaurantOrder(this.props.restaurantOrder.id,action));
     };
 
     render() {
@@ -70,105 +110,118 @@ class RestaurantOrder extends React.Component {
                     </View>
                     <View style={{width:60}}/>
                 </View>
-                <View style={s.orderDetails}>
-                    <View style={s.orderDetailsLeft}>
-                        <Text>Order ID</Text>
-                        <Text style={s.orderId}>{this.props.restaurantOrder.pickup_code}</Text>
-                    </View>
-                    <View style={s.orderDetailsRight}>
-                    <Text style={s.date}>{ moment(new Date(this.props.restaurantOrder.ordered_at)).format('ll') }</Text>
-                    <Text style={s.time}>{ moment(new Date(this.props.restaurantOrder.ordered_at)).format('LT') }</Text>
-                    </View>
-                </View>
-                <ListView style={s.orderItems}
-                          dataSource={this.ds.cloneWithRows(this.props.restaurantOrder.order_items)}
-                          enableEmptySections={true}
-                          showsVerticalScrollIndicator={true}
-                          renderRow={orderItem=>{
-                              return <RestaurantOrderItem
-                                          key={orderItem.id}
-                                          orderItem={orderItem}/>
-                          }}>
-                </ListView>
-                <View style={s.status}>
-                    <View style={s.orderStatus}>
-                        <Icon style={s.statusIcon} name={this.props.getIconForOrderStatus(this.props.orderStatus?this.props.orderStatus.name:"")} size={20} color={"#F37521"}/>
-                        <Text> {this.props.orderStatus?this.props.orderStatus.name:"Loading"} </Text>
-                    </View>
-                    {
-                        this.props.orderStatus && this.props.orderStatus.name=="Completed" &&
-                        <View style={s.paymentMethod}>
-                            <Icon style={s.statusIcon} name={this.props.getIconForPaymentMethod(this.props.paymentMethod?this.props.paymentMethod.name:"")} size={20} color={"#F37521"}/>
-                            <Text> {this.props.paymentMethod.name} </Text>
+                { !this.props.restaurantOrder && <Loading /> }
+                {
+                    this.props.restaurantOrder &&
+                    <View style={{flex:1}}>
+                        <View style={s.orderDetails}>
+                            <View style={s.orderDetailsLeft}>
+                                <Text>Order ID</Text>
+                                <Text style={s.orderId}>{this.props.restaurantOrder.pickup_code}</Text>
+                            </View>
+                            <View style={s.orderDetailsRight}>
+                            <Text style={s.date}>{ moment(new Date(this.props.restaurantOrder.ordered_at)).format('ll') }</Text>
+                            <Text style={s.time}>{ moment(new Date(this.props.restaurantOrder.ordered_at)).format('LT') }</Text>
+                            </View>
                         </View>
-                    }
-                    {
-                        this.props.orderStatus && this.props.orderStatus.name!="Completed" &&
-                        <View style={[s.statusChangeButtonWrapper,this.getStatusChangeButtonWrapperFlex()]}>
-                            <TouchableOpacity
-                                style={[s.statusChangeButton,this.getStatusChangeButtonStyle()]}
-                                onPress={()=>{}}>
-                                <Text style={s.statusButtonText}>{this.getStatusChangeButtonName()}</Text>
-                            </TouchableOpacity>
+                        { this.props.inProgress.includes(this.props.restaurantOrder.id) && <Loading /> }
+                        <ListView style={s.orderItems}
+                                  dataSource={this.ds.cloneWithRows(this.props.restaurantOrder.order_items)}
+                                  enableEmptySections={true}
+                                  showsVerticalScrollIndicator={true}
+                                  renderRow={orderItem=>{
+                                      return <RestaurantOrderItem
+                                                  key={orderItem.id}
+                                                  orderItem={orderItem}/>
+                                  }}>
+                        </ListView>
+                        <View style={s.status}>
+                            <View style={s.orderStatus}>
+                                <Icon style={s.statusIcon} name={this.getIconForOrderStatus(this.getOrderStatus()?this.getOrderStatus().name:"")} size={20} color={"#F37521"}/>
+                                <Text> {this.getOrderStatus()?this.getOrderStatus().name:"Loading"} </Text>
+                            </View>
                             {
-                                this.props.orderStatus && this.props.orderStatus.name=="Purchased" &&
-                                <TouchableOpacity
-                                    style={[s.statusChangeButton, s.rejectButton]}
-                                    onPress={() => {}}>
-                                    <Text style={s.statusButtonText}>REJECT</Text>
-                                </TouchableOpacity>
+                                this.getOrderStatus() &&
+                                (this.getOrderStatus().name=="Completed" ||
+                                this.getOrderStatus().name=="Rejected") &&
+                                <View style={s.paymentMethod}>
+                                    <Icon style={s.statusIcon} name={this.getIconForPaymentMethod(this.getPaymentMethod()?this.getPaymentMethod().name:"")} size={20} color={"#F37521"}/>
+                                    <Text> {this.getPaymentMethod().name} </Text>
+                                </View>
+                            }
+                            {
+                                this.getOrderStatus() &&
+                                this.getOrderStatus().name!="Completed" &&
+                                this.getOrderStatus().name!="Rejected" &&
+                                <View style={[s.statusChangeButtonWrapper,this.getStatusChangeButtonWrapperFlex()]}>
+                                    <TouchableOpacity
+                                        style={[s.statusChangeButton,this.getStatusChangeButtonStyle()]}
+                                        disabled={this.props.inProgress.includes(this.props.restaurantOrderId)}
+                                        onPress={()=>{this.handleAction()}}>
+                                        <Text style={s.statusButtonText}>{this.getStatusChangeButtonName()}</Text>
+                                    </TouchableOpacity>
+                                    {
+                                        this.getOrderStatus() && this.getOrderStatus().name=="Purchased" &&
+                                        <TouchableOpacity
+                                            style={[s.statusChangeButton, s.rejectButton]}
+                                            disabled={this.props.inProgress.includes(this.props.restaurantOrderId)}
+                                            onPress={() => {this.handleAction("reject")}}>
+                                            <Text style={s.statusButtonText}>REJECT</Text>
+                                        </TouchableOpacity>
+                                    }
+                                </View>
                             }
                         </View>
-                    }
-                </View>
-                <View style={s.orderStatusProgress}>
-                    <View style={[s.orderStatusProgressBar, this.getOrderStatusProgressBarStyle(0)]}/>
-                    <View style={[s.orderStatusProgressBar, this.getOrderStatusProgressBarStyle(1)]}/>
-                    <View style={[s.orderStatusProgressBar, this.getOrderStatusProgressBarStyle(2)]}/>
-                    <View style={[s.orderStatusProgressBar, this.getOrderStatusProgressBarStyle(3)]}/>
-                </View>
-                <View style={s.billing}>
-                    <View style={s.billingLeft}>
-                        <View style={s.billingRow}>
-                            <Text style={s.billingText}>Items</Text>
-                            <View style={s.priceWrapper}>
-                                <Text style={s.rupeeSymbol}>  </Text>
-                                <Text style={s.price}>{this.props.restaurantOrder.order_items.length} { this.props.restaurantOrder.order_items.length>1?"items":"item" }</Text>
-                            </View>
+                        <View style={s.orderStatusProgress}>
+                            <View style={[s.orderStatusProgressBar, this.getOrderStatusProgressBarStyle(0)]}/>
+                            <View style={[s.orderStatusProgressBar, this.getOrderStatusProgressBarStyle(1)]}/>
+                            <View style={[s.orderStatusProgressBar, this.getOrderStatusProgressBarStyle(2)]}/>
+                            <View style={[s.orderStatusProgressBar, this.getOrderStatusProgressBarStyle(3)]}/>
                         </View>
-                        <View style={s.billingRow}>
-                            <Text style={s.billingText}>Sub Total</Text>
-                            <View style={s.priceWrapper}>
-                                <Text style={s.rupeeSymbol}>₹ </Text>
-                                <Text style={s.price}>{ parseFloat(this.props.restaurantOrder.total).toFixed(2) }</Text>
+                        <View style={s.billing}>
+                            <View style={s.billingLeft}>
+                                <View style={s.billingRow}>
+                                    <Text style={s.billingText}>Items</Text>
+                                    <View style={s.priceWrapper}>
+                                        <Text style={s.rupeeSymbol}>  </Text>
+                                        <Text style={s.price}>{this.props.restaurantOrder.order_items.length} { this.props.restaurantOrder.order_items.length>1?"items":"item" }</Text>
+                                    </View>
+                                </View>
+                                <View style={s.billingRow}>
+                                    <Text style={s.billingText}>Sub Total</Text>
+                                    <View style={s.priceWrapper}>
+                                        <Text style={s.rupeeSymbol}>₹ </Text>
+                                        <Text style={s.price}>{ parseFloat(this.props.restaurantOrder.total).toFixed(2) }</Text>
+                                    </View>
+                                </View>
+                                <View style={s.billingRow}>
+                                    <Text style={s.billingText}>Packing</Text>
+                                    <View style={s.priceWrapper}>
+                                        <Text style={s.rupeeSymbol}>₹ </Text>
+                                        <Text style={s.price}>0.00</Text>
+                                    </View>
+                                </View>
                             </View>
-                        </View>
-                        <View style={s.billingRow}>
-                            <Text style={s.billingText}>Packing</Text>
-                            <View style={s.priceWrapper}>
-                                <Text style={s.rupeeSymbol}>₹ </Text>
-                                <Text style={s.price}>0.00</Text>
+                            <View style={s.billingTextSeparator}/>
+                            <View style={s.billingRight}>
+                                <Text style={s.totalText}>GRAND TOTAL</Text>
+                                <Text style={s.total}>₹ { parseFloat(this.props.restaurantOrder.total).toFixed(2) }</Text>
                             </View>
                         </View>
                     </View>
-                    <View style={s.billingTextSeparator}/>
-                    <View style={s.billingRight}>
-                        <Text style={s.totalText}>GRAND TOTAL</Text>
-                        <Text style={s.total}>₹ { parseFloat(this.props.restaurantOrder.total).toFixed(2) }</Text>
-                    </View>
-                </View>
+                }
             </View>
         );
 
     }
 
-    getStatusChangeButtonWrapperFlex = () => { return {flex:(this.props.orderStatus && this.props.orderStatus.name=="Purchased")?1.3:1}; };
+    getStatusChangeButtonWrapperFlex = () => { return {flex:(this.getOrderStatus() && this.getOrderStatus().name=="Purchased")?1.3:1}; };
     getStatusChangeButtonStyle = () => {
         let backgroundColor = '#666';
         let paddingLeft = 20;
         let paddingRight = 20;
-        if(this.props.orderStatus)
-            switch (this.props.orderStatus.name) {
+        if(this.getOrderStatus())
+            switch (this.getOrderStatus().name) {
                 case "Purchased": backgroundColor = "#669966"; paddingLeft=10; paddingRight=10; break;
                 case "Accepted": backgroundColor = "#66A055"; break;
                 case "Ready": backgroundColor = "#66AA44"; break;
@@ -178,14 +231,14 @@ class RestaurantOrder extends React.Component {
 
     getOrderStatusProgressBarStyle = (index) => {
         let backgroundColor = '#EEE';
-        if(this.props.orderStatus)
+        if(this.getOrderStatus())
             switch (index) {
-                case 0: if(this.props.orderStatus.name == "Purchased") backgroundColor = "#00a803";
-                case 1: if(this.props.orderStatus.name == "Accepted") backgroundColor = "#00a803";
-                case 2: if(this.props.orderStatus.name == "Ready") backgroundColor = "#00a803";
-                case 3: if(this.props.orderStatus.name == "Completed") backgroundColor = "#00a803";
+                case 0: if(this.getOrderStatus().name == "Purchased") backgroundColor = "#00a803";
+                case 1: if(this.getOrderStatus().name == "Accepted") backgroundColor = "#00a803";
+                case 2: if(this.getOrderStatus().name == "Ready") backgroundColor = "#00a803";
+                case 3: if(this.getOrderStatus().name == "Completed") backgroundColor = "#00a803";
             }
-        if(this.props.orderStatus.name == "Rejected") backgroundColor = "#e10000";
+        if(this.getOrderStatus() && this.getOrderStatus().name == "Rejected") backgroundColor = "#e10000";
         return ({backgroundColor: backgroundColor});
     };
 
