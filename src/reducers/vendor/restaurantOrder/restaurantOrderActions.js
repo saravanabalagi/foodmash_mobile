7b8df4c450bb8ed6fd2e480a6ffa6ae200f860a4
Zeke <saravanabalagi@hotmail.com>
2017-02-24 22:37:52 +0530

    Vendor Restaurant Actions

--- src/reducers/vendor/restaurantOrder/restaurantOrderActions.js
+++ src/reducers/vendor/restaurantOrder/restaurantOrderActions.js
@@ -24,3 +24,21 @@ export function fetchRestaurantOrder(id) {
             .catch((error) => { dispatch({type: "FETCH_RESTAURANT_ORDER_FAILED", payload: error, id: id}); });
     };
 }
+
+export function makeRestaurantOrder(id, action) {
+    let url = '/vendor/restaurant_orders/' + id.toString();
+    switch(action) {
+        case "approve": url+='/approve'; break;
+        case "reject": url+='/reject'; break;
+        case "ready": url+='/ready'; break;
+        case "collected": url+='/collected'; break;
+        default: return;
+    }
+    return (dispatch) => {
+        if(store.getState().order.inProgress.includes(id)) return;
+        dispatch({type: "FETCH_RESTAURANT_ORDER_IN_PROGRESS", id: id});
+        axios.patch(url)
+            .then((response) => { dispatch({type: "FETCH_RESTAURANT_ORDER_FULFILLED", payload: response.data, id: id}); })
+            .catch((error) => { dispatch({type: "FETCH_RESTAURANT_ORDER_FAILED", payload: error, id: id}); });
+    }
+}
