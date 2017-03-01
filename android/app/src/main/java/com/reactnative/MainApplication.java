6a5fbdaf68b3bd6655bedcd9d94d5169a68bfb19
Zeke <saravanabalagi@hotmail.com>
2017-03-02 02:18:49 +0530

    Clean upgrade to react-native 0.42

--- android/app/src/main/java/com/reactnative/MainApplication.java
+++ android/app/src/main/java/com/reactnative/MainApplication.java
@@ -1,14 +1,13 @@
 package com.reactnative;
 
 import android.app.Application;
-import android.util.Log;
 
 import com.facebook.react.ReactApplication;
 import com.zmxv.RNSound.RNSoundPackage;
-import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
+import com.facebook.soloader.SoLoader;
 
 import java.util.Arrays;
 import java.util.List;
@@ -32,6 +31,12 @@ public class MainApplication extends Application implements ReactApplication {
 
   @Override
   public ReactNativeHost getReactNativeHost() {
-      return mReactNativeHost;
+    return mReactNativeHost;
+  }
+
+  @Override
+  public void onCreate() {
+    super.onCreate();
+    SoLoader.init(this, /* native exopackage */ false);
   }
 }
